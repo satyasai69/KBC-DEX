9e04738aa3d6aa34d8b3ac536a9d8aa9d074e6d8
satyasai69 <ch.satya.sai133@gmail.com>
2024-05-05 04:00:07 +0530

    update

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { withSentryConfig } from '@sentry/nextjs'
+/* import { withSentryConfig } from '@sentry/nextjs'
 import { withAxiom } from 'next-axiom'
 import BundleAnalyzer from '@next/bundle-analyzer'
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
@@ -28,6 +28,168 @@ const sentryWebpackPluginOptions =
         silent: true, // Suppresses all logs
         dryRun: !process.env.SENTRY_AUTH_TOKEN,
       }
+
+/** @type {import('next').NextConfig} */ /*
+const config = {
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    scrollRestoration: true,
+    transpilePackages: [
+      '@pancakeswap/ui',
+      '@pancakeswap/uikit',
+      '@pancakeswap/swap-sdk-core',
+      '@pancakeswap/farms',
+      '@pancakeswap/localization',
+      '@pancakeswap/hooks',
+      '@pancakeswap/multicall',
+      '@pancakeswap/token-lists',
+      '@pancakeswap/utils',
+      '@pancakeswap/tokens',
+      '@pancakeswap/smart-router',
+      '@wagmi',
+      'wagmi',
+      '@ledgerhq',
+      '@gnosis.pm/safe-apps-wagmi',
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static-nft.pancakeswap.com',
+        pathname: '/mainnet/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/info/token/:address',
+        destination: '/info/tokens/:address',
+      },
+      {
+        source: '/info/pool/:address',
+        destination: '/info/pools/:address',
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/logo.png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/images/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/images/tokens/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, immutable, max-age=604800',
+          },
+        ],
+      },
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/send',
+        destination: '/swap',
+        permanent: true,
+      },
+      {
+        source: '/swap/:outputCurrency',
+        destination: '/swap?outputCurrency=:outputCurrency',
+        permanent: true,
+      },
+      {
+        source: '/create/:currency*',
+        destination: '/add/:currency*',
+        permanent: true,
+      },
+      {
+        source: '/farms/archived',
+        destination: '/farms/history',
+        permanent: true,
+      },
+      {
+        source: '/pool',
+        destination: '/liquidity',
+        permanent: true,
+      },
+      {
+        source: '/staking',
+        destination: '/pools',
+        permanent: true,
+      },
+      {
+        source: '/syrup',
+        destination: '/pools',
+        permanent: true,
+      },
+      {
+        source: '/collectibles',
+        destination: '/nfts',
+        permanent: true,
+      },
+      {
+        source: '/info/pools',
+        destination: '/info/pairs',
+        permanent: true,
+      },
+      {
+        source: '/info/pools/:address',
+        destination: '/info/pairs/:address',
+        permanent: true,
+      },
+    ]
+  },
+  webpack: (webpackConfig, { webpack }) => {
+    // tree shake sentry tracing
+    webpackConfig.plugins.push(
+      new webpack.DefinePlugin({
+        __SENTRY_DEBUG__: false,
+        __SENTRY_TRACING__: false,
+      }),
+    )
+    return webpackConfig
+  },
+}
+
+export default withBundleAnalyzer(withVanillaExtract(withSentryConfig(withAxiom(config), sentryWebpackPluginOptions)))
+
+*/
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { withAxiom } from 'next-axiom'
+import BundleAnalyzer from '@next/bundle-analyzer'
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
+
+const withBundleAnalyzer = BundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+// const withTM = NextTranspileModules([])
+
+const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
 const config = {
@@ -174,4 +336,4 @@ const config = {
   },
 }
 
-export default withBundleAnalyzer(withVanillaExtract(withSentryConfig(withAxiom(config), sentryWebpackPluginOptions)))
+export default withBundleAnalyzer(withVanillaExtract(withAxiom(config)))
